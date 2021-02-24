76db28594e7f94ff0826fe242e37ebb2edb15bff
jay-flow <kimpetro153@gmail.com>
2021-02-24 15:26:45 +0900

    [Fix] Eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
     "plugin:prettier/recommended"
   ],
   parser: "@typescript-eslint/parser",
@@ -19,20 +18,12 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module"
   },
-  plugins: ["react", "@typescript-eslint", "prettier/@typescript-eslint"],
+  plugins: ["react", "@typescript-eslint"],
   rules: {
     "prettier/prettier": ["error", { endOfLine: "auto" }, { usePrettierrc: true }],
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/no-var-requires": "off",
     "react/prop-types": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "jsx-a11y/anchor-is-valid": [
-      "error",
-      {
-        components: ["Link"],
-        specialLink: ["hrefLeft", "hrefRight"],
-        aspects: ["invalidHref", "preferButton"]
-      }
-    ]
+    "@typescript-eslint/explicit-function-return-type": "off"
   }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
+import { ReactElement } from "react"
 
-function App() {
+function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
